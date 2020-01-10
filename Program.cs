bce326759b9fd65de0ba24fa8c3d3efede06118c
CamilleLozenge <57403391+CamilleLozenge@users.noreply.github.com>
2020-01-10 15:12:57 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RemoveLzgRegistryKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string keyLzg = @"Software\Lzg";
+            if (Registry.CurrentUser.OpenSubKey(keyLzg) != null)
+            {
+                Registry.CurrentUser.DeleteSubKey(keyLzg);
+            }
+            
+        }
+    }
+}
