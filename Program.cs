ed6d6e9dfae23182339a8eff28851331b8c0da33
CamilleLozenge <57403391+CamilleLozenge@users.noreply.github.com>
2020-01-13 09:07:12 +0100

    commit 13012020

--- Program.cs
+++ Program.cs
@@ -1,21 +1,30 @@
-﻿using Microsoft.Win32;
+﻿using LicLZXL.Interfaces;
+using LicLZXL.Singleton;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 
 namespace RemoveLzgRegistryKey
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            string keyLzg = @"Software\Lzg";
-            if (Registry.CurrentUser.OpenSubKey(keyLzg) != null)
-            {
-                Registry.CurrentUser.DeleteSubKey(keyLzg);
-            }
-            
+            var checkLicence = CheckLicenceSingleton.GetInstance();
+   
+            HttpWebRequest webRequest = (HttpWebRequest)HttpWebRequest.Create(string.Format(
+                "http://www.lozenge-co.com/ltt/licenceReport.php?lic={0}&user={1}&host={2}",
+                checkLicence.DecodeData(checkLicence.GetRegistryKeyValue("LzLiN")),
+                checkLicence.GetUserMachine(),
+                checkLicence.GetMachineName()));
+                
+            HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
+
         }
     }
 }
